4e1356ed132e4068374d42db32b7511e1acebd2b
hojun9459 <144601971+hojun9459@users.noreply.github.com>
2023-12-06 02:12:55 +0900

    시작 화면 추가

--- hojun/src/hojun/MiroGame.java
+++ hojun/src/hojun/MiroGame.java
@@ -10,17 +10,23 @@ import java.util.TimerTask;
 public class MiroGame extends JFrame {
     private MyPanel panel = new MyPanel();
     private int playerX = 10;
-    private int playerY = 685;
+    private int playerY = 720;
     private final int playerWidth = 55;
     private final int playerHeight = 75;
     private Timer timer;
     private long startTime;
+    private JPanel startPanel;
+    private long elapsedTime;
 
     public MiroGame() {
         setTitle("바뀐 시험장으로 찾아가자!");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        // Start button to begin the game
+
+        ImageIcon loadingImageIcon = new ImageIcon("images/loading2.jpg");
+        loadingImageIcon = new ImageIcon(loadingImageIcon.getImage().getScaledInstance(1536, 864, Image.SCALE_DEFAULT));
+
+        JLabel loadingLabel = new JLabel(loadingImageIcon);
+
         JButton startButton = new JButton("시작하기");
         startButton.addActionListener(new ActionListener() {
             @Override
@@ -28,35 +34,34 @@ public class MiroGame extends JFrame {
                 startGame();
             }
         });
-        
-        // Create a panel for the start button
-        JPanel startPanel = new JPanel();
-        startPanel.add(startButton);
-        
-        // Add the start panel and the game panel
+
+        startPanel = new JPanel(new BorderLayout());
+        startPanel.add(loadingLabel, BorderLayout.CENTER);
+        startPanel.add(startButton, BorderLayout.SOUTH);
+
         setLayout(new BorderLayout());
-        add(startPanel, BorderLayout.NORTH);
-        add(panel, BorderLayout.CENTER);
+        add(startPanel, BorderLayout.CENTER);
 
         setSize(2560, 1440);
         setVisible(true);
     }
 
     private void startGame() {
-        panel.requestFocusInWindow(); // Set focus to the game panel
-        startTime = System.currentTimeMillis(); // Record the start time
+        startPanel.setVisible(false);
+        add(panel, BorderLayout.CENTER);
+        panel.requestFocusInWindow();
+        panel.setVisible(true); 
+        startTime = System.currentTimeMillis();
 
-        // Create and start a timer
         timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                // Update the elapsed time
                 long elapsedTime = System.currentTimeMillis() - startTime;
                 DecimalFormat df = new DecimalFormat("0.00");
                 setTitle("바뀐 시험장으로 찾아가자! - 경과 시간: " + df.format(elapsedTime / 1000.0) + "초");
             }
-        }, 0, 1000); // Update every second
+        }, 0, 1000);
 
         panel.addKeyListener(new KeyListener() {
             @Override
@@ -86,7 +91,6 @@ public class MiroGame extends JFrame {
                         break;
                 }
 
-                // Check for collision before updating the player's position
                 if (!checkCollision(newPlayerX, newPlayerY)) {
                     playerX = newPlayerX;
                     playerY = newPlayerY;
@@ -105,23 +109,23 @@ public class MiroGame extends JFrame {
     private boolean checkCollision(int x, int y) {
         Rectangle playerBounds = new Rectangle(x, y, playerWidth, playerHeight);
 
-        Rectangle building1Bounds = new Rectangle(610, 460, 480, 280);
-        Rectangle building2Bounds = new Rectangle(10, 395, 480, 280);
-        Rectangle building3Bounds = new Rectangle(520, 75, 480, 280);
-        Rectangle building4Bounds = new Rectangle(1000, 75, 480, 280);
-        Rectangle building5Bounds = new Rectangle(1090, 460, 480, 280);
+        Rectangle building1Bounds = new Rectangle(610, 480, 480, 280);
+        Rectangle building2Bounds = new Rectangle(10, 425, 480, 280);
+        Rectangle building3Bounds = new Rectangle(520, 90, 480, 280);
+        Rectangle building4Bounds = new Rectangle(1000, 90, 480, 280);
+        Rectangle building5Bounds = new Rectangle(1090, 480, 480, 280);
 
         if (playerBounds.intersects(building1Bounds) ||
             playerBounds.intersects(building2Bounds) ||
             playerBounds.intersects(building3Bounds) ||
             playerBounds.intersects(building4Bounds)) {
             playerX = 10;
-            playerY = 685;
+            playerY = 720;
             panel.repaint();
             return true;
         } else if (playerBounds.intersects(building5Bounds)) {
             timer.cancel(); // Stop the timer
-            long elapsedTime = System.currentTimeMillis() - startTime;
+            elapsedTime = System.currentTimeMillis() - startTime;
             DecimalFormat df = new DecimalFormat("0.00");
             JOptionPane.showMessageDialog(panel, "미션 성공했습니다!\n걸린 시간: " + df.format(elapsedTime / 1000.0) + "초");
             return true;
@@ -144,13 +148,17 @@ public class MiroGame extends JFrame {
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
             g.drawImage(img, 0, 0, getWidth(), getHeight(), this);
-            g.drawImage(Building1.getImage(), 610, 460, 480, 280, this);
-            g.drawImage(Building2.getImage(), 10, 395, 480, 280, this);
-            g.drawImage(Building3.getImage(), 520, 75, 480, 280, this);
-            g.drawImage(Building4.getImage(), 1000, 75, 480, 280, this);
-            g.drawImage(Building5.getImage(), 1090, 460, 480, 280, this);
+            g.drawImage(Building1.getImage(), 610, 480, 480, 280, this);
+            g.drawImage(Building2.getImage(), 10, 425, 480, 280, this);
+            g.drawImage(Building3.getImage(), 520, 90, 480, 280, this);
+            g.drawImage(Building4.getImage(), 1000, 90, 480, 280, this);
+            g.drawImage(Building5.getImage(), 1090, 480, 480, 280, this);
             g.drawImage(playerIcon.getImage(), playerX, playerY, playerWidth, playerHeight, this);
         }
+    }
+    
+    public long getScore() {
+    	return elapsedTime;
     }
 
     public static void main(String[] args) {
