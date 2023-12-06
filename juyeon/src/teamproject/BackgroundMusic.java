cd7420681b3f9db7341310ab944ccf37308625f9
ppakw <ppakw@10.50.10.17>
2023-12-07 01:06:44 +0900

    song

--- juyeon/src/teamproject/BackgroundMusic.java
+++ juyeon/src/teamproject/BackgroundMusic.java
@@ -8,7 +8,7 @@ public class BackgroundMusic {
     public static void main(String[] args) throws UnsupportedAudioFileException, LineUnavailableException, IOException, InterruptedException {
         String musicFilePath = "image/videoplayback.wav"; // 음악 파일 경로 설정
 
-        // musicFile 만듦
+        // musicFile 만듦qwqwqw
         File musicFile = new File(musicFilePath);
 
         // 음악 파일을 AudioInputStream으로 읽어들여야 오디오 파일을 읽고, 오디오 데이터를 Clip에 제공
