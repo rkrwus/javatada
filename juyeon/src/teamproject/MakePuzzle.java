fa431657d4b02d32fd95a227b63cd7000ff96f39
jukite <144404574+jukite@users.noreply.github.com>
2023-12-06 21:43:16 +0900

    Add files via upload
    panel로 바꾼 버전

--- juyeon/src/teamproject/MakePuzzle.java
+++ juyeon/src/teamproject/MakePuzzle.java
@@ -1,6 +1,5 @@
 package teamproject;
 
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -8,25 +7,18 @@ import java.awt.image.*;
 import java.io.*;
 import javax.imageio.ImageIO;
 
-public class MakePuzzle extends JFrame {
+public class MakePuzzle extends JPanel {
     private BufferedImage[][] original = new BufferedImage[3][3];
     private BufferedImage image;
     private JTextField textField;
     private long startTime;
     private long endTime;
-    private int time;
+    private int timeInSeconds;
 
     public MakePuzzle() {
-        initializeUI();
-    }
-
-    private void initializeUI() {
-        setTitle("Puzzle");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(2560, 1440);
+        setLayout(new BorderLayout());
 
         JPanel imagePanel = new JPanel() {
-            @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
                 if (image != null) {
@@ -47,7 +39,11 @@ public class MakePuzzle extends JFrame {
 
         JButton startButton = new JButton("시작하기");
         startButton.setPreferredSize(new Dimension(200, 50));
-        startButton.addActionListener(e -> startPuzzleGame());
+        startButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                startPuzzleGame();
+            }
+        });
         startButtonPanel.add(startButton);
 
         imagePanel.add(startButtonPanel, BorderLayout.CENTER);
@@ -75,7 +71,7 @@ public class MakePuzzle extends JFrame {
     }
 
     private void startPuzzleGame() {
-        getContentPane().removeAll();
+        removeAll();
         revalidate();
 
         try {
@@ -95,19 +91,7 @@ public class MakePuzzle extends JFrame {
             }
         }
 
-        JPanel puzzlePanel = createPuzzlePanel();
-        add(puzzlePanel, BorderLayout.CENTER);
-
-        JPanel inputPanel = createInputPanel();
-        add(inputPanel, BorderLayout.SOUTH);
-
-        startTime = System.currentTimeMillis();
-        revalidate();
-    }
-
-    private JPanel createPuzzlePanel() {
-        return new JPanel() {
-            @Override
+        JPanel puzzlePanel = new JPanel() {
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
                 int width = image.getWidth() / 3;
@@ -125,53 +109,52 @@ public class MakePuzzle extends JFrame {
                 }
             }
         };
-    }
+        add(puzzlePanel, BorderLayout.CENTER);
 
-    private JPanel createInputPanel() {
         JPanel inputPanel = new JPanel();
         JLabel label = new JLabel("메모의 정답(특수기호, 띄어쓰기 제외):");
         textField = new JTextField(20);
         JButton submitButton = new JButton("정답 제출");
 
-        submitButton.addActionListener(e -> checkAnswerAndCalculateTime());
+        submitButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String inputText = textField.getText();
+                if (inputText.equals("교수님사랑합니다")) {
+                    checkAnswerAndCalculateTime();
+                    JOptionPane.showMessageDialog(null, "정답입니다람쥐.");
+                } else {
+                    JOptionPane.showMessageDialog(null, "틀렸습니다. 다시 시도해주세요.");
+                }
+            }
+        });
 
         inputPanel.add(label);
         inputPanel.add(textField);
         inputPanel.add(submitButton);
+        add(inputPanel, BorderLayout.NORTH);
 
-        return inputPanel;
+        startTime = System.currentTimeMillis();
+
+        revalidate();
     }
 
     private void checkAnswerAndCalculateTime() {
         endTime = System.currentTimeMillis();
+
         long timeTaken = endTime - startTime;
-        int timeInSeconds = (int) (timeTaken / 1000);
 
-        String inputText = textField.getText();
-        if (inputText.equals("교수님사랑합니다")) {
-            showSuccessMessage(timeInSeconds);
-        } else {
-            showErrorMessage();
-        }
+        timeInSeconds = (int) (timeTaken / 1000);
     }
 
-    private void showSuccessMessage(int timeInSeconds) {
-        JOptionPane.showMessageDialog(null, "정답입니다! 소요된 시간: " + timeInSeconds + " 초");
+    private int getScore() {
+        return timeInSeconds;
     }
 
-    private void showErrorMessage() {
-        JOptionPane.showMessageDialog(null, "틀렸습니다. 다시 시도해주세요.");
-    }
-
-    private int getscore() {
-    	endTime = System.currentTimeMillis();
-    	long timeTaken = endTime-startTime;
-    	return time = (int)(timeTaken/1000);
-    }
     private int[] generateRandomArray() {
         int[] randomNumbers = new int[9];
         boolean[] used = new boolean[9];
         int count = 0;
+
         while (count < 9) {
             int randomNumber = (int) (Math.random() * 9);
             if (!used[randomNumber]) {
@@ -180,10 +163,20 @@ public class MakePuzzle extends JFrame {
                 count++;
             }
         }
+
         return randomNumbers;
     }
 
     public static void main(String[] args) {
-    	new MakePuzzle();
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                JFrame frame = new JFrame("Puzzle");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setSize(2560, 1440);
+                frame.add(new MakePuzzle());
+                frame.setVisible(true);
+            }
+        });
     }
 }
+
