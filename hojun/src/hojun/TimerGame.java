e3950f26faecb416f2e22ce86e15ad89a821e9f4
hojun9459 <144601971+hojun9459@users.noreply.github.com>
2023-12-06 20:48:11 +0900

    Real Pannel

--- hojun/src/hojun/TimerGame.java
+++ hojun/src/hojun/TimerGame.java
@@ -2,7 +2,8 @@ package hojun;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class TimerGame extends JPanel {
     private JPanel startPanel;
@@ -16,10 +17,10 @@ public class TimerGame extends JPanel {
     private double totalPlaytime;
 
     public TimerGame() {
-    	
+        setLayout(new BorderLayout());
 
         ImageIcon loadingImageIcon = new ImageIcon("images/loading1.jpg");
-        loadingImageIcon = new ImageIcon(loadingImageIcon.getImage().getScaledInstance(1280, 720, Image.SCALE_DEFAULT));
+        loadingImageIcon = new ImageIcon(loadingImageIcon.getImage().getScaledInstance(1536, 864, Image.SCALE_DEFAULT));
 
         JLabel loadingLabel = new JLabel(loadingImageIcon);
 
@@ -35,13 +36,13 @@ public class TimerGame extends JPanel {
         startPanel.add(loadingLabel, BorderLayout.CENTER);
         startPanel.add(startButton, BorderLayout.SOUTH);
 
-        setLayout(new BorderLayout());
         add(startPanel, BorderLayout.CENTER);
     }
 
     private void startGame() {
-    	gamePanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        //setContentPane(gamePanel);
+        gamePanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        setLayout(new BorderLayout());
+        add(gamePanel, BorderLayout.CENTER);
         revalidate();
 
         ImageIcon gameImageIcon = new ImageIcon("images/GameImage.jpg");
@@ -50,16 +51,16 @@ public class TimerGame extends JPanel {
         JLabel gameImageLabel = new JLabel(gameImageIcon);
         gameImageLabel.setBounds(0, 0, 1280, 720);
         gamePanel.add(gameImageLabel);
-        
+
         timerLabel = new JLabel("0");
         timerLabel.setFont(new Font("TimesRoman", Font.ITALIC, 250));
         timerLabel.setBounds(900, -100, getWidth(), getHeight());
         gameImageLabel.add(timerLabel);
-        
+
         stopButton = new JButton("스탑");
         stopButton.setBounds(900, 450, 300, 100);
         gameImageLabel.add(stopButton);
-        
+
         stopButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -67,7 +68,6 @@ public class TimerGame extends JPanel {
             }
         });
 
-        
         startTime = System.currentTimeMillis();
 
         gameTimer = new Timer(10, new ActionListener() {
@@ -91,7 +91,8 @@ public class TimerGame extends JPanel {
 
             if (elapsedTime >= 8.8 && elapsedTime <= 9.0) {
                 JOptionPane.showMessageDialog(this, "알람 설정에 성공했습니다!\n총 플레이 타임: " + String.format("%.1f", totalPlaytime) + " 초");
-                //setContentPane(gamePanel);
+                setLayout(new BorderLayout());
+                add(gamePanel, BorderLayout.CENTER);
                 revalidate();
             } else {
                 int option = JOptionPane.showConfirmDialog(this, "시간 내에 멈추지 못했습니다!\n재시도 하시겠습니까?", "Retry", JOptionPane.YES_NO_OPTION);
@@ -100,7 +101,8 @@ public class TimerGame extends JPanel {
                     startTime = System.currentTimeMillis();
                     gameTimer.start();
                 } else {
-                    //setContentPane(startPanel);
+                    setLayout(new BorderLayout());
+                    add(startPanel, BorderLayout.CENTER);
                     revalidate();
                 }
             }
@@ -108,6 +110,17 @@ public class TimerGame extends JPanel {
     }
 
     public int getScore() {
-        return (int)totalPlaytime;
+        return (int) totalPlaytime;
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("알람을 맞춰라!");
+        frame.setSize(2560, 1440);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        TimerGame timerGame = new TimerGame();
+        frame.add(timerGame);
+
+        frame.setVisible(true);
     }
 }
