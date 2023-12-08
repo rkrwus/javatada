dc3b081390fcc3152f4e0b676bfdc8bfc2995360
ihojun <ihojun@10.50.10.197>
2023-12-08 11:17:26 +0900

    퍼즐 게임 작업 중

--- hojun/src/hojun/MainSystem.java
+++ hojun/src/hojun/MainSystem.java
@@ -107,7 +107,7 @@ public class MainSystem extends JFrame {
 	void operateRankingSystem() {		
 		totalScore = firstScore + secondScore + thirdScore + fourthScore;
 		totalScore = convertTimeToScore(totalScore);
-		new RankingSystem(this, 480); // 수정 필요
+		new RankingSystem(this, totalScore); // 수정 필요
 	}
 
 	int convertTimeToScore(int time) {
@@ -133,7 +133,7 @@ public class MainSystem extends JFrame {
 		playBtn.setFont(new Font("Mistral", Font.PLAIN, 50));
 		playBtn.setForeground(new Color(25, 77, 51));
 		playBtn.setBackground(new Color(240, 248, 255));
-		playBtn.setBounds(465, 430, 500, 80);
+		playBtn.setBounds(380, 330, 500, 80);
 //		playBtn.setBorderPainted(false);
 		
 		imgPanel.add(playBtn);
